c0f5cdf872bdeda4729b9926f8bc26b6972f777c
BNgeniuz <137963261+BNgeniuz@users.noreply.github.com>
2023-11-15 23:44:25 +0000

    looping

--- loops.c
+++ loops.c
@@ -107,7 +107,7 @@ void find_cmd(info_t *info)
 	else
 	{
 		if ((interactive(info) || _getenv(info, "PATH=")
-					|| info->argv[0][0] == '/') && my_cmd(info, info->argv[0]))
+					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_cmd(info);
 		else if (*(info->arg) != '\n')
 		{
