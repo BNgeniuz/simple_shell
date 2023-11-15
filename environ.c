db1d2fd059fbda774e5c45096df817971b6435d8
Godwin Bruce <godwin.bruce@gmail.com>
2023-11-15 02:15:42 -0800

    environ

--- environ.c
+++ environ.c
@@ -1,36 +1,91 @@
 #include "shell.h"
 
 /**
-  *print_env - Print the current environment
-  */
-void print_env(void)
+ * _myenv - prints curr environ
+ * @info: Struct contain potential arg used to maintain valid prototype.
+ *
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
 {
-	extern char **environ;
-	int i = 0;
-
-	while (environ[i] != NULL)
-	{
-		write(1, environ[i], strlen(environ[i]));
-		write(1, "\n", 1);
-		i++;
-	}
+	print_list_str(info->env);
+	return (0);
 }
 
 /**
-  *main - Entry point of the program
-  *
-  *Return: Always 0
-  */
-int main(void)
+ * _getenv - inputs environ variable value
+ * @info: Struct contain potential arg used to maintain valid prototype
+ * @name: environ name
+ *
+ * Return: 0 (environment)
+ */
+char *_getenv(info_t *info, const char *name)
 {
-	char *input = NULL;
-	size_t bufsize = 0;
+	list_t *node = info->env;
+	char *q;
 
-	while (1)
+	while (node)
 	{
-		write(1, "$", 2);
-		input = getline(&input, $bufsize, stdin);
-		return (input);
+		q = starts_with(node->str, name);
+		if (q && *q)
+			return (q);
+		node = node->next;
 	}
-	retrun (0);
+	return (NULL);
+}
+
+/**
+ * _mysetenv - new environ var init or modify an existing one
+ * @info: Struct containi potential arg used to maintain valid prototype
+ *
+ *  Return: 0 (environment)
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - delete environ var
+ * @info: Struct contain potential arg used to maintain validprototype
+ *
+ * Return: 0 (environ)
+ */
+int _myunsetenv(info_t *info)
+{
+	int l;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (l = 1; l <= info->argc; l++)
+		_unsetenv(info, info->argv[l]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - duplicate env linked list
+ * @info: Struct contain potential arg used to maintain prototype
+ *
+ * Return: 0 (environ)
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t l;
+
+	for (l = 0; environ[l]; l++)
+		add_node_end(&node, environ[l], 0);
+	info->env = node;
+	return (0);
 }
