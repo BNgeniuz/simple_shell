a71f506e2d23e2b90652aa3c36fa3a46531a7e23
BNgeniuz <137963261+BNgeniuz@users.noreply.github.com>
2023-11-15 18:13:49 +0000

    main shell files

--- main.c
+++ main.c
@@ -1,26 +1,44 @@
 #include "shell.h"
 
 /**
-  * main - shell barsh program
-  * @argc: the argument counter
-  * @argv: vector of array for input
-  * @environ: variables to declare for envirment
-  *
-  * Return: Always success
-  */
-
-int main(int argc, char *argv[], char **environ)
+ * main - input entry points
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (main file)
+ */
+int main(int argc, char **argv)
 {
-	char *buffer_input;
-	(void)argc;
+	info_t info[] = { INFO_INIT };
+	int fdx = 2;
 
-	while (1)
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fdx)
+			: "r" (fdx));
+
+	if (argc == 2)
 	{
-		print_prompt();
-		buffer_input = read_input();
-		execute_command(buffer_input, argv, environ);
-		free(buffer_input);
+		fdx = open(argv[1], O_RDONLY);
+		if (fdx == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(argv[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(argv[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fdx;
 	}
-
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, argv);
+	return (EXIT_SUCCESS);
 }
